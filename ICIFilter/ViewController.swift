e2764532a8572b43980f1a61217bfe33e9510db3
ixialuo <ixialuo@gmail.com>
2018-12-20 09:48:02 +0800

    Initial Commit

---
+++ ICIFilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ICIFilter
+//
+//  Created by xialuo on 2018/12/20.
+//  Copyright © 2018 https://ixialuo.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
